dcce41a2ab44f94427d22031ba3257411019945f
Auros Nexus <aurosnex@gmail.com>
2021-02-23 21:20:24 -0500

    Initial commit

---
+++ Bepinject/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+
+namespace Bepinject
+{
+    [BepInPlugin("dev.auros.bepinex.bepinject", "Bepinject", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        protected void Awake()
+        {
+
+        }
+
+        protected void OnDestroy()
+        {
+
+        }
+    }
+}
